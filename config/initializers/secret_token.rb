6cb214e6088199c2d8acd8a3b4ea0ad86cf54085
swalke16 <swalke16@gmail.com>
2011-07-19 10:54:01 -0400

    started rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeubauerPhoto::Application.config.secret_token = '42a466595e1bc49595f8df21e9df23311660a2292462fb734e397223493b0cb12827c0530c7d2d823221b41f8d15995238720797c1bdf816059455068effdf54'
