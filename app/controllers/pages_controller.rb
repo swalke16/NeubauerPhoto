1a9515ac628c6ab38bc6c6209d5bebb1c046a2ac
swalke16 <swalke16@gmail.com>
2011-09-12 21:40:17 -0400

    index styling mostly done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
+
+  before_filter :get_photosets
+
   def gallery
-    flickr = Flickr.new('05ad56341ed719ab0c1d65aca564c863')
-    @photosets = flickr.users('eneubauer').photosets
   end
 
   def about
@@ -11,6 +12,13 @@ class PagesController < ApplicationController
   end
 
   def home
+  end
+
+  private
+
+  def get_photosets
+    flickr = Flickr.new('05ad56341ed719ab0c1d65aca564c863')
+    @photosets = flickr.users('eneubauer').photosets.sort {|x,y| x.title <=> y.title }
   end
 
 end
