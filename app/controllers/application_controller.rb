fc0bc0a524999bda12bcbcae685b24e6f1759597
swalke16 <swalke16@gmail.com>
2011-09-17 23:20:12 -0400

    stubbing out about and contact pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :get_photosets
+
+
   protect_from_forgery
+
+  private
+
+  def get_photosets
+    flickr = Flickr.new('05ad56341ed719ab0c1d65aca564c863')
+    @photosets = flickr.users('eneubauer').photosets.sort {|x,y| x.title <=> y.title }
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
 
-  before_filter :get_photosets
-
   def gallery
   end
 
@@ -12,13 +10,6 @@ class PagesController < ApplicationController
   end
 
   def home
-  end
-
-  private
-
-  def get_photosets
-    flickr = Flickr.new('05ad56341ed719ab0c1d65aca564c863')
-    @photosets = flickr.users('eneubauer').photosets.sort {|x,y| x.title <=> y.title }
   end
 
 end
